98f73b69f7005f3c4602a0160914d08afbacf187
dkaygithub <dkaygithub@users.noreply.github.com>
2021-08-28 23:37:43 -0500

    Script Encoding
    Exhibit encoding of luascript from source code into json

--- src/main.go
+++ src/main.go
@@ -13,24 +13,31 @@ var (
 	path = flag.String("path", "testdata/simple", "a directory containing tts mod configs")
 )
 
+const (
+	luaSubdir = "luascript"
+)
+
 type Config struct {
-	Name      string `json:"Name"`
-	Version   string `json:"version"`
-	ImagePath string `json:"ImagePath"`
-	ObjectDir string
+	Name           string `json:"Name"`
+	Version        string `json:"version"`
+	ImagePath      string `json:"ImagePath"`
+	LuaScriptPath  string `json:"LuaScriptPath"`
+	LuaScriptState string `json:"LuaScriptState"`
+	ObjectDir      string
 }
 
 // Mod is used as the accurate representation of what gets printed when
 // module creation is done
 type Mod struct {
-	SaveName     string
-	EpochTime    int64
-	Date         string
-	Tags         []string
-	LuaScript    string
-	Decals       []*Decal
-	ObjectStates []*Object
-	SnapPoints   []*SnapPoint
+	SaveName       string
+	EpochTime      int64
+	Date           string
+	Tags           []string
+	LuaScript      string
+	LuaScriptState string
+	Decals         []*Decal
+	ObjectStates   []*Object
+	SnapPoints     []*SnapPoint
 }
 
 type Decal struct {
@@ -51,7 +58,7 @@ func main() {
 		fmt.Printf("readConfig(%s) : %v\n", *path, err)
 		return
 	}
-	m, err := generateMod(c)
+	m, err := generateMod(*path, c)
 	if err != nil {
 		fmt.Printf("generateMod(<config>) : %v\n", err)
 		return
@@ -72,8 +79,10 @@ func readConfig(cPath string) (*Config, error) {
 	// defer the closing of our jsonFile so that we can parse it later on
 	defer cFile.Close()
 
-	b, _ := ioutil.ReadAll(cFile)
-
+	b, err := ioutil.ReadAll(cFile)
+	if err != nil {
+		return nil, fmt.Errorf("ioutil.Readall(%s) : %v", cPath+"/config.json", err)
+	}
 	var c Config
 
 	err = json.Unmarshal(b, &c)
@@ -83,13 +92,19 @@ func readConfig(cPath string) (*Config, error) {
 	return &c, nil
 }
 
-func generateMod(c *Config) (*Mod, error) {
+func generateMod(p string, c *Config) (*Mod, error) {
 	if c == nil {
 		return nil, fmt.Errorf("nil config")
 	}
 	var m Mod
 
 	m.SaveName = c.Name
+	encoded, err := encodeLuaScript(p, c.LuaScriptPath)
+	if err != nil {
+		return nil, fmt.Errorf("encodeLuaScript(%s) : %v", c.LuaScriptPath, err)
+	}
+	m.LuaScript = encoded
+	m.LuaScriptState = c.LuaScriptState
 
 	return &m, nil
 }
@@ -101,4 +116,21 @@ func printMod(p string, m *Mod) error {
 	}
 
 	return ioutil.WriteFile(p+"/output.json", b, 0644)
+}
+
+func encodeLuaScript(p, f string) (string, error) {
+	path := p + "/" + luaSubdir + "/" + f
+	sFile, err := os.Open(path)
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		return "", fmt.Errorf("os.Open(%s): %v", path, err)
+	}
+	defer sFile.Close()
+
+	b, err := ioutil.ReadAll(sFile)
+	if err != nil {
+		return "", err
+	}
+
+	return string(b), nil
 }
